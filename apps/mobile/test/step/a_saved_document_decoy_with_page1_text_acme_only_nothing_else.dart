35cf41f1f201b6bbe04f33109b9f515a86cb35e9
pablohpsilva <pablohpsilva@proton.me>
2026-07-05 21:37:05 +0200

    cleanup

---
+++ apps/mobile/test/step/a_saved_document_decoy_with_page1_text_acme_only_nothing_else.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+/// Usage: a saved document "Decoy" with page 1 text "acme only, nothing else"
+Future<void> aSavedDocumentDecoyWithPage1TextAcmeOnlyNothingElse(
+    WidgetTester tester) async {
+  throw UnimplementedError();
+}
